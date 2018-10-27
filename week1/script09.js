0153ea0b7cd4b526d5bb13d725df8d40ac27e2c1
Maen Al Mahameed <41008566+maa3n@users.noreply.github.com>
2018-10-27 22:07:21 +0200

    Create script09.js

---
+++ week1/script09.js
@@ -0,0 +1,27 @@
+let xx = 7;
+xx = xx % 3;
+console.log("The value of my variable is : " + xx );
+/* 7/3=2
+   2*3=6
+   7-6=1 */
+
+let q = 45;
+q = q % 8;
+console.log("The value of my variable is : " + q );
+/* 45/8=5
+   5*8=40
+   45-40=5 */
+
+let s = 75;
+s = s % 6;
+console.log("The value of my variable is : " + s );
+/* 75/6=12
+   12*6=72
+   75-72=3 */
+
+let v = 85;
+v = v % 11;
+console.log("The value of my variable is : " + v );
+/* 85/11=7
+   7*11=77
+   85-77=8 */
