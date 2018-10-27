1c22134f65f9626d3559cf5c38ae6154dd774947
Maen Al Mahameed <41008566+maa3n@users.noreply.github.com>
2018-10-27 22:04:59 +0200

    Create script06.js

---
+++ week1/script06.js
@@ -0,0 +1,8 @@
+const zoo = [];
+console.log ('There is No Value for zoo');
+console.log(zoo[0]);
+
+const myFavoriteAnimals = ['Penguin','Giraffe','tiger'];
+console.log(myFavoriteAnimals [0]);
+console.log(myFavoriteAnimals [1]);
+console.log(myFavoriteAnimals [2]);
