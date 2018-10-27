0c63e524e5deae9c76cf12be5bb223de78966d67
Maen Al Mahameed <41008566+maa3n@users.noreply.github.com>
2018-10-27 22:08:07 +0200

    Create script10.js

---
+++ week1/script10.js
@@ -0,0 +1,15 @@
+
+let myList =[ 28 , "pen" ];
+console.log(myList);
+console.log('Yes I can store multiple types in one array : ' + typeof myList [0]);
+console.log('Yes I can store multiple types in one array : ' + typeof myList [1]);
+
+
+let y = 6/0
+let z = 10/0
+
+if (y===z) {
+  console.log('true Y and Z have the Same Value')
+} else {
+  console.log('false Y and Z have a different Value')
+}
