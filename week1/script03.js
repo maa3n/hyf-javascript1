e4ad7481f8a597a47774bb81dcc337308a4f10aa
Maen Al Mahameed <41008566+maa3n@users.noreply.github.com>
2018-11-27 11:28:49 +0100

    Update script03.js

--- week1/script03.js
+++ week1/script03.js
@@ -2,5 +2,5 @@ let x
 console.log("the value of my variable x will be undefined");
 console.log(x);
 x = 100
-console.log("the value of my variable x will be 100");
-console.log(x);
+console.log("the value of my variable x will be " + x);
+
